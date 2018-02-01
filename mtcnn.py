1072dd19d2d9fc8de9fb380615cd7905a063c3d1
邬小阳 <1441615466@qq.com>
2018-02-01 08:57:07 +0800

    mtcnn的pythonban版本，实现人脸的检测和关键点检测

---
+++ mtcnn.py
@@ -0,0 +1,9 @@
+#/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+'''
+@date: 2018-02-01
+@brief: this file is for MTCNN in python version. 
+        All models are released in the repository of MTCNN's official repository.
+        https://github.com/kpzhang93/MTCNN_face_detection_alignment
+'''
